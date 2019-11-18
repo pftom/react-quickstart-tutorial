517171b1a03f5b931e61de99bd2f75eab7a18bd0
pftom <1043269994@qq.com>
2019-11-18 22:34:37 +0800

    JSX 语法

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from "react-dom";
 
 class App extends React.Component {
   render() {
-    return <div>Hello, World</div>;
+    const todoList = ["图雀", "图雀写作工具", "图雀社区", "图雀文档"];
+    return (
+      <ul>
+        <li>Hello, {todoList[0]}</li>
+        <li>Hello, {todoList[1]}</li>
+        <li>Hello, {todoList[2]}</li>
+        <li>Hello, {todoList[3]}</li>
+      </ul>
+    );
   }
 }
 
