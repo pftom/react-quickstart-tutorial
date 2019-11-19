fa3833376ae2d3a898937017a64b8289a9120c19
pftom <1043269994@qq.com>
2019-11-19 15:17:11 +0800

    Component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+class Todo extends React.Component {
+  render() {
+    return <li>Hello, 图雀</li>;
+  }
+}
+
 class App extends React.Component {
   render() {
     const todoList = ["图雀", "图雀写作工具", "图雀社区", "图雀文档"];
     return (
       <ul>
-        <li>Hello, {todoList[0]}</li>
-        <li>Hello, {todoList[1]}</li>
-        <li>Hello, {todoList[2]}</li>
-        <li>Hello, {todoList[3]}</li>
+        <Todo />
+        <Todo />
+        <Todo />
+        <Todo />
       </ul>
     );
   }
